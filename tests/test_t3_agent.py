716c054651c6efcca8b9893c215e29591c764eac
Shubham Dhapola <dhapola.shubham321@gmail.com>
2023-01-05 19:10:59 +0530

    Update 'tests/' with test for tic-tac-toe playing agent

---
+++ tests/test_t3_agent.py
@@ -0,0 +1,9 @@
+import bots.t3_agent as agent
+
+# ------------------ GAME RENDERING CHECK ---------------------
+#
+# Check how the grid renders when the script is run:
+#   - direct invocation from the command prompt.
+#   - when imported from inside the `demo/` folder.
+# 
+# -------------------------------------------------------------
